88bd0172c8c21e4d391e187cb33d6495250c0ecd
Donald Akite <donaldakite27@gmail.com>
2023-03-07 18:08:49 +0000

    Add student class

---
+++ Student.rb
@@ -0,0 +1,16 @@
+require_relative "Person"
+
+class Students < Person
+
+    attr_accessor :classroom
+  
+    def initialize(id,age,classroom,name="unknown",parent_permission=true)
+        super(id,age,name,parent_permission)
+        @classroom = classroom
+    end
+
+    def play_hooky
+       return  "¯\(ツ)/¯"
+    end
+
+end
