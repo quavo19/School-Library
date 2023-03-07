7256955db98c6706ebfb32830a62670847d903e3
Donald Akite <donaldakite27@gmail.com>
2023-03-07 18:08:32 +0000

    Add person class

---
+++ Person.rb
@@ -0,0 +1,25 @@
+class Person
+    attr_reader :id, :name, :age
+    attr_writer :name, :age
+    def initialize(id,age,name="unknown",parent_permission=true)
+        @id = id
+        @parent_permission = parent_permission
+        @name = name
+        @age = age
+    end
+
+    private
+
+    def is_of_age? 
+        @age >= 10
+    end
+
+    
+    public
+
+    def can_use_services?
+        is_of_age? || parent_permission = true
+    end
+    
+
+end
