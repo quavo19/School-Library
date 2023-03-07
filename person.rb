c53f0455c155df2201d9ff06a813469486e6e64a
Donald Akite <donaldakite27@gmail.com>
2023-03-07 19:03:32 +0000

    Update person.rb

--- person.rb
+++ person.rb
@@ -11,7 +11,7 @@ class Person
 
   private
 
-  def is_of_age?
+  def of_age?
     @age >= 10
   end
 
@@ -19,6 +19,6 @@ class Person
   public
 
   def can_use_services?
-    is_of_age? || parent_permission
+    of_age? || parent_permission
   end
 end
