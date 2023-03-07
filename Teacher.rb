eb095eca5d2bce67819b8cf74ab1f77b5ee6cda6
Donald Akite <donaldakite27@gmail.com>
2023-03-07 18:09:17 +0000

    Add Teacher class

---
+++ Teacher.rb
@@ -0,0 +1,15 @@
+require_relative 'Person'
+
+class Teacher < Person
+    attr_accessor :specialization
+  
+    def initialize(id,age,specialization,name="unknown",parent_permission=true)
+        super(id,age,name,parent_permission)
+        @specialization = specialization
+    end
+
+    def can_use_services?
+        return true
+    end
+
+end
